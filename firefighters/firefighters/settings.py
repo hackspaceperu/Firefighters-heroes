bc06d20fa6543679bd118b28b2043b4bed9a3224
Jack Moreno <jack12972@gmail.com>
2017-10-22 03:02:28 -0500

    modify setting

--- firefighters/firefighters/settings.py
+++ firefighters/firefighters/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'firefighters',
     'accounts',
     'vehicle',
+    'emergency',
     'rest_framework',
     'corsheaders'
 ]
@@ -82,8 +83,15 @@ WSGI_APPLICATION = 'firefighters.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'firefighters',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '0.0.0.0',   # Or an IP Address that your DB is hosted on
+        'PORT': '8000',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
